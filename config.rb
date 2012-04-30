973290ec1c6728fb9784bdc9a78192f82af9a313
Vince Speelman <V@VinSpee.me>
2012-04-30 15:31:36 -0400

    Removed @font-face created stylesheet in SASS used Compass mixins where applicable created variables for colors for easy modification

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "/"
+javascripts_dir = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+sass_options = {:debug_info=>true}
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
+
+output_style = :compact # by Fire.app 
+
+sass_options = {:debug_info=>false} # by Fire.app 
+line_comments = false # by Fire.app 
